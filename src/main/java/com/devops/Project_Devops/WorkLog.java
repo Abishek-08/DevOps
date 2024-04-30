892a45c4506bf80e2f37ea49a35481023a0f9a01
Abishek <abik2781@gmail.com>
2024-04-30 10:09:47 +0530

    First commit

---
+++ src/main/java/com/devops/Project_Devops/WorkLog.java
@@ -0,0 +1,14 @@
+package com.devops.Project_Devops;
+
+public class WorkLog {
+	
+	public String addToLog(String name) {
+		return "Added Log For "+name;
+		
+		}
+	
+	public String addLogins(String loginName) {
+		return "Added Login For "+loginName;
+	}
+
+}
